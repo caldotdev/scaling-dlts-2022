4f5321a8f01cc4a8ff478cc550a541c9e8485414
Cal Dotio <caldotio@protonmail.com>
2022-05-22 11:08:04 +0200

    feat(layout): :sparkles: image-right layout with contained background

---
+++ layouts/contained-image-right.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { handleBackground } from "../node_modules/slidev-theme-academic/layout-helper";
+
+const props = defineProps({
+  image: {
+    type: String,
+  },
+  class: {
+    type: String,
+  },
+});
+const style = computed(() => handleBackground(props.image));
+</script>
+
+<template>
+  <div class="grid grid-cols-2 w-full h-full">
+    <div class="slidev-layout default" :class="props.class">
+      <slot />
+    </div>
+    <div class="w-full w-full bg" :style="style" />
+  </div>
+</template>
+
+<style scoped>
+.bg {
+    background-size: contain !important;
+    margin-top: 2.5rem;
+    margin-bottom: 2.5rem;
+}
+
+</style>
